58ce4bb1fb359ff29319eceb1ec7bb3a803c1d58
Nit89 <nitesh382singh.com>
2023-11-02 14:48:47 +0530

    additional feature

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface Task {
+  taskname: string;
+  isCompleted: boolean;
+  priority: string;
+}
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
   styleUrls: ['./todolist.component.css'],
 })
 export class TodolistComponent {
-  taskArray = [{ taskname: 'Brush teeth', isCompleted: false }];
+  taskArray = [
+    { taskname: 'Brush teeth', isCompleted: false, priority: 'low' },
+  ];
   onSubmit(form: NgForm) {
+    console.log(this.taskArray);
     this.taskArray.push({
       taskname: form.controls['task'].value,
       isCompleted: false,
+      priority: form.controls['priority'].value,
     });
     form.reset();
   }
@@ -21,5 +30,11 @@ export class TodolistComponent {
   onCheck(index: number) {
     console.log(this.taskArray);
     this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
+
+  markAllAsCompleted() {
+    this.taskArray.forEach((task) => {
+      task.isCompleted = true;
+    });
   }
 }
